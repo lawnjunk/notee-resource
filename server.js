96a3c7f9953cdaba03b495a2d4f7cd24b7caa2fb
Duncan Marsh <dwolfm@gmail.com>
2015-07-06 21:12:24 -0700

    added a notes model

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 'use strict';
 
+var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
+
+var MONGOLAB_URI = process.env.MONGOLAB_URI || 'mongodb://localhost/notes_dev'
+
+mongoose.connect(MONGOLAB_URI);
 
 var port = process.env.PORT || 3000;
 
